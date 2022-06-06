3d3e1a8c43237b7259c5658db7a0aaa77de1ae7b
Amos Koech <koechronix@yahoo.com>
2022-06-05 21:05:59 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+//for loop assignment
+const names = ["Guadalupe", "Ollie" ,"Ali"];{
+    event = "suprise";
+}
+function writeCards(names, event) {
+    let thankYouCards = [];
+    for (let i = 0; i < names.length; i++){
+        thankYouCards.push(
+            `Thank you, ${names[i]}, for the wonderful ${event}gift!`
+        );
+    }
+    return thankYouCards();
+}
+
+function countDown(startInt) {
+    while (startInt>0){
+        console.log(startInt);
+        startInt -=1;
+    }
+    console.log(startInt);
+}
